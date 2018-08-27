41d09dba9863a8f7c66c4dfdddf2591c29c3ede8
sophia <sophia.zhu10@gmail.com>
2018-08-27 15:29:28 +1000

    loops_arrays

---
+++ .loops_arrays.rb
@@ -0,0 +1,14 @@
+
+puts 'how do you go to class and how long does it take?'
+
+# bar_store=['cocktails',  'waters', 'beers']
+
+# puts 'what would you like to drink?'
+# order = gets.chomp
+#  if order == '1 cocktail'
+#     puts bar_store+1
+
+     
+#  elsif 
+     
+#  end
--- 02_loops_arrays.rb
+++ 02_loops_arrays.rb
@@ -2,18 +2,47 @@
 # - create a complete sentence  that lets us know how you got to class and how long it took
 # - print this complete sentence
 
+# timetoarrive= '1 hour'
+# puts " It takes me #{timetoarrive} to get here by bus."
+
 # you are working at a bar where you have a current backlog of drinks to make:
 # 3 cocktails
 # 2 waters
 # and
 # 6 beers
 
+
 # write a program that asks the customer for their order.
 # if they order a cocktail, add one to the number of cocktails you need to make,
 # if they order a water, add one to the number of waters you need to make,
 # if they order a beer, add one to the number of beers you need to pour
 
-# print the final drinks order so you know what to make
+#print the final drinks order so you know what to make
+array=[3, 2, 6]
+# cocktail=3
+# waters=2
+# beers=6
+order=""
+until order == 'nothing' do
+    puts 'what would you like to drink?'
+    order= gets.chomp
+    case order 
+        when "cocktail"
+            array[0]+=1
+        when "water"
+            array[1]+=1
+        when "beer"
+            array[2]+=1
+        else 
+            puts "cheers!"
+    end
+end
+puts "i need to make #{array[0]} cocktails, #{array[1]} waters, #{array[2]} beers "
+  
+
+    
+
+
 
 # ****
 # cocktails sell for $22, and cost $8 to make
@@ -21,3 +50,18 @@
 # water sell for $6, and cost $0.15 to make
 
 # print out the total profit for the orders you have
+cocktailprofit= 14 * array[0].to_i
+waterprofit=9 * array[1].to_i
+beerprofit=5.85 * array[2].to_f
+ totalprofit= cocktailprofit + waterprofit +beerprofit
+ totalprofit = totalprofit.round(2)
+ print "the total profit is #{totalprofit}"
+
+# print "#{cocktailprofit} + #{waterprofit} + #{beerprofit}"
+
+
+    
+
+ 
+
+
