91a3cc8c6eb91f67e865cee75ccd2a1e0209d5b0
sophia <sophia.zhu10@gmail.com>
2018-09-11 09:02:34 +1000

    new update

--- 11_vowels.rb
+++ 11_vowels.rb
@@ -13,7 +13,15 @@
 # Check your solution by running the tests:
 # ruby tests/04_vowels_test.rb
 #
+require 'test/unit'
+require_relative '../04_vowels'
 
-def vowels (string)
-  # Your code here
+class LargestNumberTest < Test::Unit::TestCase
+  def test_vowels
+    assert_equal(["e","u","i","o","o"], vowels("The quick brown fox"))
+    assert_equal(["e","o","o"], vowels("Hello World"))
+  end
+  def test_vowels_casing
+    assert_equal(["A","e","E"], vowels("cAse tEst"))
+  end
 end
