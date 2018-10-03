1ff36c53b6f2504f1f4622e84f166b5231519b90
sophia <sophia.zhu10@gmail.com>
2018-10-04 09:58:36 +1000

    21

--- 21_page_title.rb
+++ 21_page_title.rb
@@ -15,17 +15,7 @@ require 'nokogiri' # gem install nokogiri (if you don't have it installed)
 require 'open-uri'
 
 def pageTitle(url)
-<<<<<<< HEAD
-  # your code here
-  require 'watir'
-browser = Watir::Browser.new
-browser.goto('url')
-puts browser.title
-browser.close
-=======
   # Fetch and parse HTML document
   doc = Nokogiri::HTML(open(url))
   doc.search('title').children[0].text
->>>>>>> d785f49315f21edd5329234273e9431f9c199eea
 end
-pageTitle('http://google.com')
